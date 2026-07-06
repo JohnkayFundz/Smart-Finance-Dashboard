6d94b79772f453f6b66ca303488015b9b262d91f
John Kalumba <deejayjohnkay@gmail.com>
2026-07-06 15:28:13 -0700

    Refactor isFocusable function to use matches method

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function isFocusable(element) {
-    const style = getComputedStyle(element);
-
-    return (
-        !element.disabled &&
-        !element.hidden &&
-        !element.hasAttribute("inert") &&
-        element.getAttribute("aria-hidden") !== "true" &&
-        style.display !== "none" &&
-        style.visibility !== "hidden"
-    );
-}
+return (
+    !element.matches(":disabled") &&
+    !element.hidden &&
+    !element.hasAttribute("inert") &&
+    element.getAttribute("aria-hidden") !== "true" &&
+    style.display !== "none" &&
+    style.visibility !== "hidden"
+);
