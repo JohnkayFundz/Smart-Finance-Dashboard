8ee77a3c9d46180fde7b2e3f85915cfef7fe71a2
John Kalumba <deejayjohnkay@gmail.com>
2026-07-06 15:14:34 -0700

    Refactor getFocusableElements for visibility checks
    Refactor getFocusableElements function to use computed styles for visibility checks.

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-function getFocusableElements(modal) {
-    return [...modal.querySelectorAll(`
-        a[href],
-        button:not([disabled]),
-        textarea:not([disabled]),
-        input:not([disabled]),
-        select:not([disabled]),
-        [tabindex]:not([tabindex="-1"])
-    `)].filter(element => {
-        return (
-            !element.hasAttribute("disabled") &&
-            element.getAttribute("aria-hidden") !== "true" &&
-            element.offsetParent !== null
-        );
-    });
-}
+const style = window.getComputedStyle(element);
+
+return (
+    !element.hasAttribute("disabled") &&
+    element.getAttribute("aria-hidden") !== "true" &&
+    style.display !== "none" &&
+    style.visibility !== "hidden"
+);
